4f5b9e170bc3ee225576d40a67aed738ed8b411c
Stack-queue2023 <stackqueue2023@gmail.com>
2023-08-19 10:04:33 +0530

    manikandan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ManiComponent } from './mani/mani.component';
+import { JeevaComponent } from './jeeva/jeeva.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ManiComponent
+    ManiComponent,
+    JeevaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/jeeva/jeeva.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jeeva',
+  templateUrl: './jeeva.component.html',
+  styleUrls: ['./jeeva.component.css']
+})
+export class JeevaComponent {
+
+}
